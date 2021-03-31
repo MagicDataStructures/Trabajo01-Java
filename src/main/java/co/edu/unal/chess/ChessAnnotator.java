e4d3d057a1d2d37289167b35eb66a25dbb27e591
KernelFreeze <kernelfreeze@outlook.com>
2021-03-30 20:11:14 -0500

    Añadiendo selector de blancas y negras

--- src/main/java/co/edu/unal/chess/ChessAnnotator.java
+++ src/main/java/co/edu/unal/chess/ChessAnnotator.java
@@ -1,7 +1,42 @@
 package co.edu.unal.chess;
 
-public class ChessAnnotator {
-    public static void main(String[] args) {
+import java.util.Scanner;
+import java.util.logging.Logger;
 
+public class ChessAnnotator {
+    private final Logger logger = Logger.getLogger(ChessAnnotator.class.getName());
+
+    private String whiteName;
+    private String blackName;
+
+    public static void main(String[] args) {
+        var in = new Scanner(System.in);
+        var annotator = new ChessAnnotator();
+
+        annotator.getLogger().info("Ingrese el nombre del que juega blancas");
+        annotator.setWhiteName(in.nextLine());
+
+        annotator.getLogger().info("Ingrese el nombre del que juega negras");
+        annotator.setBlackName(in.nextLine());
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+
+    public String getWhiteName() {
+        return whiteName;
+    }
+
+    public void setWhiteName(String whiteName) {
+        this.whiteName = whiteName;
+    }
+
+    public String getBlackName() {
+        return blackName;
+    }
+
+    public void setBlackName(String blackName) {
+        this.blackName = blackName;
     }
 }
