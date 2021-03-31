8059cbc318571935b66a49790c8d7f86f541c3ff
KernelFreeze <kernelfreeze@outlook.com>
2021-03-30 20:14:24 -0500

    Fixeando UTF8

--- src/main/java/co/edu/unal/chess/ChessAnnotator.java
+++ src/main/java/co/edu/unal/chess/ChessAnnotator.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class ChessAnnotator {
+
     private final Logger logger = Logger.getLogger(ChessAnnotator.class.getName());
 
     private String whiteName;
